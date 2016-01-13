765abcad2ae6d88330e77bf2ac3ff3e356dba012
leticialima <lodldesigner@gmail.com>
2016-01-13 14:48:33 -0300

    primeiro comite para subir o projeto sweetlove para o github

---
+++ js/myjquery.js
@@ -0,0 +1,23 @@
+	
+$(function() {
+  $('#slides').slidesjs({
+    width: 940,
+    height: 528,
+    navigation: {
+      active: false,
+      effect: "fade"
+    },
+    pagination: {
+      active: false,
+      effect: "fade"
+    },
+    effect: {
+      fade: {
+        speed: 100
+      }
+    }
+  });
+});
+
+  
+		
